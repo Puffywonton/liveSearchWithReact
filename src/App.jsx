cd95f48aa07e23c4dd50ffd7ab983926b0290548
Puffywonton <wontonking123@outlook.com>
2023-06-02 01:43:48 +0200

    base working

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
 import LiveSearchContainer from './LiveSearchContainer'
 import axios from 'axios';
 
 
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
   const [characters, setCharacters] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log('trying')
-        const { data } = await axios.get('https://swapi.dev/api/people/')
-        setCharacters(data.results)
-        console.log('done')
-      } catch (error) {
-        console.log(error)
-      }
+  const fetchData = async (searchTerm) => {
+    try {
+      console.log('trying')
+      const url = 'https://swapi.dev/api/people/?search=' + searchTerm
+      console.log(url)
+      const { data } = await axios.get(url)
+      setCharacters(data.results)
+      console.log('done')
+    } catch (error) {
+      console.log(error)
     }
-    fetchData()
-  }, [])
-
-  const handleSearch = (searchTerm) => {
-    setSearchResults(searchTerm)
-    setTimeout(coucou, 1000)
-  };
-
-  const coucou = () => {
-    console.log('coucou')
   }
-
-
+    
   return (
     <>
       <h1>SWAPI Live Search</h1>
-      <LiveSearchContainer onSearch={handleSearch} />
+      <LiveSearchContainer onSearch={fetchData} />
       <div className="results">
         {characters.map(character => (
           <div key={character.name}>
